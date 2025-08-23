02f2a711923265f2c14b4b056f226561a4fb2d31
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-23 22:13:20 +0530

    done the status part

--- src/main/java/com/aditya/aditya/Status.java
+++ src/main/java/com/aditya/aditya/Status.java
@@ -3,15 +3,20 @@ package com.aditya.aditya;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Status {
+
     @GetMapping("/status")
-    public Map<String, String> StatusCode(){
-        Map<String , String> response = new HashMap<>();
-        response.put("status", "400");
-        return response;
+    public ResponseEntity<Map<String, String>> StatusCode() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Bad Request Example");
+
+        // Return status code 400 with the response
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
