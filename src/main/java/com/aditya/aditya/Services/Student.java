23ac8cc9f508fb21650e374badc0b541c6eeba49
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-24 17:09:22 +0530

    controller

--- src/main/java/com/aditya/aditya/Services/Student.java
+++ src/main/java/com/aditya/aditya/Services/Student.java
@@ -1,10 +1,12 @@
 package com.aditya.aditya.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.aditya.aditya.entity.StudentEntity;
 import com.aditya.aditya.repository.StudentRepository;
 
+@Component
 public class Student {
  @Autowired
  private   StudentRepository StudentRepository;
--- src/main/java/com/aditya/aditya/controller/StudentController.java
+++ src/main/java/com/aditya/aditya/controller/StudentController.java
@@ -1,5 +1,21 @@
 package com.aditya.aditya.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aditya.aditya.Services.Student;
+import com.aditya.aditya.entity.StudentEntity;
+
+@RequestMapping
 public class StudentController {
-    
+    @Autowired    
+    private Student student;
+
+    @PostMapping
+    public boolean saveStudentData(@RequestBody StudentEntity studentData){
+        student.saveStudent(studentData);
+        return true;
+    }
 }
