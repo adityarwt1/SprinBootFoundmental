9f533390976194e58bd9d2f2f6b1b43fef27851c
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-24 16:41:04 +0530

    done the entity part

---
+++ src/main/java/com/aditya/aditya/entity/Student.java
@@ -0,0 +1,23 @@
+package com.aditya.aditya.entity;
+
+public class Student {
+    ///decalare the stundent like this
+    private long  id;
+    private String name;
+    private String fathername;
+
+
+    public long getId(){
+        return id;
+    }
+
+    public String getStudentName(){
+        return name;
+    }
+
+
+    public String getFathername(){
+        return fathername;
+    }
+
+}
