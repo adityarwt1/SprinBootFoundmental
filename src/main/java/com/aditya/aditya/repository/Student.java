48ba3a2f612c3018f64bcfaa0b6361059c0bf898
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-24 16:45:50 +0530

    repostiory is workingn as well

--- src/main/java/com/aditya/aditya/repository/Student.java
+++ src/main/java/com/aditya/aditya/repository/Student.java
@@ -2,6 +2,6 @@ package com.aditya.aditya.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface Student extends MongoRepository{
+public interface Student extends MongoRepository<com.aditya.aditya.entity.Student,String  >{
     
 }
