41d43b01b0de7ad1d1c4541041e917ca2f153676
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-24 16:35:54 +0530

    time to follow the folder struncture

---
+++ src/main/java/com/aditya/aditya/Services/Student.java
@@ -0,0 +1,5 @@
+package com.aditya.aditya.Services;
+
+public class Student {
+    
+}
---
+++ src/main/java/com/aditya/aditya/repository/Student.java
@@ -0,0 +1,7 @@
+package com.aditya.aditya.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface Student extends MongoRepository{
+    
+}
