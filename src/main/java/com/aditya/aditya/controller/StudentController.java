9a5b4ae62bb02a77bb812ea041eedd9c27f59bc9
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-24 17:25:20 +0530

    student controller

--- src/main/java/com/aditya/aditya/controller/StudentController.java
+++ src/main/java/com/aditya/aditya/controller/StudentController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.aditya.aditya.Services.Student;
 import com.aditya.aditya.entity.StudentEntity;
 
-@RequestMapping
+@RestController
+@RequestMapping("/student")
 public class StudentController {
     @Autowired    
     private Student student;
