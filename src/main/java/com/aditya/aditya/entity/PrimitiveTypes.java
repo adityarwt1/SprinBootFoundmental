f01bee87d74d89e984a70a0f60bf2b7485979f2c
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-25 22:39:41 +0530

    done the entity part

---
+++ src/main/java/com/aditya/aditya/entity/PrimitiveTypes.java
@@ -0,0 +1,22 @@
+package com.aditya.aditya.entity;
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 20000;
+        int i = 1000000;
+        long l = 10000000000L;
+        float f = 3.14f;
+        double d = 3.14159265359;
+        char c = 'A';
+        boolean flag = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + flag);
+    }
+}
